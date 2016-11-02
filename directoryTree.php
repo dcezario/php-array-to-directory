79e3784187bfe63d7d909604da31d54e23a0db5f
Diego Cezario <dcezariooo@gmail.com>
2016-11-02 21:30:03 -0200

    Commit inicial

---
+++ directoryTree.php
@@ -0,0 +1,58 @@
+<?php
+$paths = array(
+	'nivel',
+	'nivel2'=>array(
+		'subnivel1',
+		'subnivel2'=>array(
+			'subnivel3',
+			'subnivel4'=>array(
+				'subsubnivel4'
+			)
+		)
+	)
+);
+
+class DirectoryTree {
+
+	private $array;
+	public function __construct($array){
+		$this->array = $array;
+	}
+	private function recursiveArray(){
+		$iterator = new RecursiveIteratorIterator(new RecursiveArrayIterator($this->array));
+		$parent = '';
+		$arrayTree = array();
+		foreach ($iterator as $key=>$value){
+			for ($i = 0; $i < $iterator->getDepth();$i++){
+					$iteratorKey = $iterator->getSubIterator($i)->key();
+						$parent.= $iteratorKey.DIRECTORY_SEPARATOR;
+						
+			}
+			if (empty($parent)){
+				$arrayTree[] = $iterator->getSubIterator($i)->current();
+			}else{
+				$arrayTree[] = $parent;
+				$arrayTree[] = $parent.$iterator->getSubIterator($i)->current();
+			}
+			//$arrayTree[$parent]= $iterator->getSubIterator($i)->current();
+			$parent = '';
+		}
+		return $arrayTree;
+	}
+	public function createDirectoryTree(){
+		$arrayTree = $this->recursiveArray();
+		if (is_array($arrayTree)){
+			foreach ($arrayTree as $key=>$value){
+				if(!is_dir($value)){
+					mkdir($value,0777,true);
+				}
+			}
+			return true;
+		}
+		throw new Exception("Incorrect array format");
+	}
+}
+$directoryTree = new DirectoryTree($paths);
+echo "<pre>";
+var_dump($directoryTree->createDirectoryTree());
+echo "</pre>";
